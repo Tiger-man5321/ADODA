81c9360df51ead88dccca89e4364fe0057dd3632
Apple <apple@Apples-MacBook-Pro.local>
2022-08-06 12:11:33 +0700

    Initial commit

---
+++ ac-dOM.js
@@ -0,0 +1,15 @@
+// let count=0;
+// function changIMG(){
+//     if(count%2 === 0) {
+//         var g = document.getElementById('logo');
+//         g.setAttribute('src', 'icon.png');
+//         count++;
+//     }
+//     else{
+//         var g = document.getElementById('logo');
+//         g.setAttribute('src', 'logo%20.png');
+//         count++;
+//     }
+// }
+//
+
